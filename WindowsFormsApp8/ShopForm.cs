3d397572d1ad2737f8c684481cbbaefc4f46830f
Anthony Carlos Zayas <tug92199@temple.edu>
2019-12-14 23:50:48 -0500

    fixed inserts

--- WindowsFormsApp8/ShopForm.cs
+++ WindowsFormsApp8/ShopForm.cs
@@ -161,6 +161,7 @@ namespace BookCDDVDShop
                     newCDOrchestral.Save(this);
 
                     //Inserts a new classical and orcenstral entry into database
+                    productDB.InsertProduct(newCDOrchestral.ProductUPC, newCDOrchestral.ProductPrice, newCDOrchestral.ProductTitle, newCDOrchestral.ProductQuantity, newCDOrchestral.GetType().Name);
                     productDB.InsertCDClassical(newCDOrchestral.ProductUPC, newCDOrchestral.CDClassicalLabel, newCDOrchestral.CDClassicalArtists);
                     productDB.InsertCDOrchestra(newCDOrchestral.ProductUPC, newCDOrchestral.CDOrchestralConductor);
 
@@ -177,6 +178,7 @@ namespace BookCDDVDShop
                     newCDChamber.Save(this);
 
                     //Inserts a new classical and chamber entry into database
+                    productDB.InsertProduct(newCDChamber.ProductUPC, newCDChamber.ProductPrice, newCDChamber.ProductTitle, newCDChamber.ProductQuantity, newCDChamber.GetType().Name);
                     productDB.InsertCDClassical(newCDChamber.ProductUPC, newCDChamber.CDClassicalLabel, newCDChamber.CDClassicalArtists);
                     productDB.InsertCDChamber(newCDChamber.ProductUPC, newCDChamber.CDChamberInstruments);
                     //pList.addProduct(newCDChamber);
